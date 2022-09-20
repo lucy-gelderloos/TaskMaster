c2ff7b3810329c5d6e189e81a44c145b12bd892b
Lucy Gelderloos <lucygel@gmail.com>
2022-09-19 23:11:37 -0700

    Create main activity, add task activity

--- app/src/main/java/com/gelderloos/taskmaster/MainActivity.java
+++ app/src/main/java/com/gelderloos/taskmaster/MainActivity.java
@@ -2,7 +2,11 @@ package com.gelderloos.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+    Button addTaskButton = MainActivity.this.findViewById(R.id.mainActivityButtonAddTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddTaskActivity = new Intent(MainActivity.this, AddTaskActivity.class);
+                // Launch the intent
+                startActivity(goToAddTaskActivity);
+            }});
+
+//    public void submitTask(View view) {
+//        Intent intent = new Intent(this, AddTaskActivity.class);
+//        EditText editTextTitle = (EditText) findViewById(R.id.editTextTextTaskTitle);
+//        EditText editTextDescription = (EditText) findViewById(R.id.editTextTextTaskDescription);
+//        String taskTitle = editTextTitle.getText().toString();
+//        String taskDescription = editTextDescription.getText().toString();
+//        startActivity(intent);
+//    }
+
+
+
 }
+    }
