82920be59c362c23928b366b962f53eff1610f26
Lucy Gelderloos <lucygel@gmail.com>
2022-10-04 19:54:41 -0700

    Add S3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "S3taskmaster": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- app/src/main/java/com/gelderloos/taskmaster/TaskMasterAmplifyApplication.java
+++ app/src/main/java/com/gelderloos/taskmaster/TaskMasterAmplifyApplication.java
@@ -7,6 +7,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class TaskMasterAmplifyApplication extends Application {
     public static final String Tag = "TaskMasterApplication";
@@ -18,6 +19,7 @@ public class TaskMasterAmplifyApplication extends Application {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
             Amplify.configure(getApplicationContext());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
         } catch(AmplifyException ae) {
             Log.e(Tag, "Error initializing Amplify: " + ae.getMessage(), ae);
         }
